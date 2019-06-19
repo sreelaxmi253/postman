b327506511713d10cdca371ce411da4298aef004
sreelaxmi253 <sreelaxmi253@gmail.com>
2019-06-19 20:43:33 +0530

    first commit

---
+++ postman/src/postman/PostMan.java
@@ -0,0 +1,18 @@
+package postman;
+
+public class PostMan {
+	public static void main(String[] args)
+	{
+		System.out.println("{\r\n" + 
+				"    \"data\": {\r\n" + 
+				"        \"id\": 2,\r\n" + 
+				"        \"email\": \"janet.weaver@reqres.in\",\r\n" + 
+				"        \"first_name\": \"Janet\",\r\n" + 
+				"        \"last_name\": \"Weaver\",\r\n" + 
+				"        \"avatar\": \"https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg\"\r\n" + 
+				"    }\r\n" + 
+				"}");
+	}
+	
+
+}
